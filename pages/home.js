abd77b8115183fbd19c2735250c2cc4803b978bc
Guillermo Fernandez <bengallme@gmail.com>
2021-08-21 18:39:18 -0500

    Started responsive layour for home page

--- pages/home.js
+++ pages/home.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useReactiveVar } from "@apollo/client";
 import { isLoggedVar } from "../cache";
+import Link from "next/link";
 
 export default function Home() {
   const router = useRouter();
@@ -14,10 +15,29 @@ export default function Home() {
   }, [isLogged, router]);
 
   return (
-    <div>
-      <h1 className="text-6xl text-white">
-        This will be the home page, at some point
-      </h1>
+    <div className="max-w-screen-xl grid grid-cols-4 mx-auto border-8 border-green-500 h-screen grid-rows-6 md:grid-rows-none">
+      <div className="text-6xl text-white border-white border row-start-6 md:row-start-1 md:col-span-1 col-span-4">
+        <nav className="text-3xl items-center md:items-stretch flex md:flex-col border-blue-400 border-4 justify-around h-full md:h-auto">
+          <Link href="/home">
+            <a className="my-4 hidden md:block">🦜Briefly</a>
+          </Link>
+          <Link href="/home">
+            <a className="rounded-full p-4 nav-element">Home</a>
+          </Link>
+          <Link href="/#">
+            <a className="rounded-full p-4 nav-element">Explore</a>
+          </Link>
+          <Link href="/#">
+            <a className="rounded-full p-4 nav-element">Settings</a>
+          </Link>
+        </nav>
+      </div>
+      <div className="text-6xl text-white border-white border col-span-4 md:col-span-3 lg:col-span-2 row-span-5 md:row-auto">
+        Main content goes here
+      </div>
+      <div className="text-6xl text-white border-white border hidden lg:block">
+        Ad space maybe or dunno
+      </div>
     </div>
   );
 }
